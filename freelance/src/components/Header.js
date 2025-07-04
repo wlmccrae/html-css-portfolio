4153c36879f0672710d5660f61ecb739d9d72bed
Wanda McCrae <wanda.mccrae@gmail.com>
2025-07-04 18:45:31 -0400

    Header, footer, and main components

---
+++ freelance/src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <div class="header">
+      <h1>Wanda McCrae</h1>
+      <h2>Freelance Web Developer</h2>
+      <div class="contact">
+        <a href="index.html">Home</a>
+        <a href="samples.html">Sample Sites</a>
+        <a href="contact.html">Contact</a>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
