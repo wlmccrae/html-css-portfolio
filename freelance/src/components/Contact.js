28192723de12161f8d17aba173805b589ac8b7bd
Wanda McCrae <wanda.mccrae@gmail.com>
2025-07-07 21:24:29 -0400

    Styling Contact page.

--- freelance/src/components/Contact.js
+++ freelance/src/components/Contact.js
@@ -22,7 +22,8 @@ function Contact() {
         <div className="contact-page-title">Contact Me</div>
       </div>
       <div className="contact-instructions">
-        Enter your contact information in the form, and I will do my best to respond within 48 hours.
+        <p>Have a project in mind? Need a fast, modern, or user-friendly website? I'd love to hear about it! Fill out the form below, and I'll get back to you within 48 hours to discuss how we can bring your ideas to life.</p>
+        <p>Let's create something great&mdash;reach out today!</p>
       </div>
       <form id="contact-form" className="contact-form">
         <div className="contact-form-row">
