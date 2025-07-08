18b8a7dc45232be903a4260f4932636fc5c5d2a6
Wanda McCrae <wanda.mccrae@gmail.com>
2025-07-07 21:09:31 -0400

    Contact form (not sending)

--- freelance/src/components/Contact.js
+++ freelance/src/components/Contact.js
@@ -1,11 +1,45 @@
+import {useState} from 'react';
 import '../styles/Contact.css';
 
 function Contact() {
+  const [contactFormData, setContactFormData] = useState({
+        name: "",
+        email: "",
+        message: "",
+    });
+
+    // Handle when the form changes
+    const handleChange = (event) => {
+        setContactFormData({
+            ...contactFormData,
+            [event.target.name]: event.target.value
+        });
+    };
+
   return (
-    <div>
-      <div className="title-container">
-        <div className="page-title">Contact Me</div>
+    <div className="contact-page">
+      <div className="contact-title-container">
+        <div className="contact-page-title">Contact Me</div>
       </div>
+      <div className="contact-instructions">
+        Enter your contact information in the form, and I will do my best to respond within 48 hours.
+      </div>
+      <form id="contact-form" className="contact-form">
+        <div className="contact-form-row">
+          <label for="name">Your Name<span className="required-field">*</span>: </label>
+          <input className="form-input" onChange={handleChange} type="text" id="name" name="name" />
+        </div>
+        <div className="contact-form-row">
+          <label for="email">Your Email<span className="required-field">*</span>: </label>
+          <input className="form-input" onChange={handleChange} type="text" id="email" name="email" />
+        </div>
+        <label for="name">Message<span className="required-field">*</span>:</label>
+        <input className="form-input message-field" onChange={handleChange} type="text" id="message" name="message" />
+        <p><span className="required-field">*</span>Required field.</p>
+      </form>
+      <button className="contact-me">
+        Send Message
+      </button>
     </div>
   )
 
