35701c32cfffad5a12a80ae623d886b27e32c7f4
Wanda McCrae <wanda.mccrae@gmail.com>
2025-07-08 16:02:11 -0400

    Minor style tweaks

--- freelance/src/components/Contact.js
+++ freelance/src/components/Contact.js
@@ -22,7 +22,7 @@ function Contact() {
         <div className="freelance-page-title">Contact Me</div>
       </div>
       <div className="contact-instructions">
-        <p>Have a project in mind? Need a fast, modern, or user-friendly website? I'd love to hear about it! Fill out the form below, and I'll get back to you within 48 hours to discuss how we can bring your ideas to life.</p>
+        <p>Have a project in mind? Need a fast, modern, or user-friendly website? I'd love to hear about it! Fill out the form below, and I'll get back to you within 72 hours to discuss how we can bring your ideas to life.</p>
         <p>Let's create something great&mdash;reach out today!</p>
       </div>
       <form id="contact-form" className="contact-form">
--- freelance/src/components/Samples.js
+++ freelance/src/components/Samples.js
@@ -11,12 +11,12 @@ function Samples() {
           <a
             className="sample-project-link"
             href="https://wandamccrae.com/projects/tea-shoppe/" target="_blank" rel="noopener noreferrer">
-            🪷 Lotus Blossom Tea Shoppe
+            - Lotus Blossom Tea Shoppe
           </a>
           <p>Two-page HTML/CSS site for a local tea shoppe.</p>
         </div>
         <div className="sample">
-          <a className="sample-project-link" href="https://wandamccrae.com/projects/ronni-davis/" target="_blank" rel="noopener noreferrer">🩷 Ronni Davis</a>
+          <a className="sample-project-link" href="https://wandamccrae.com/projects/ronni-davis/" target="_blank" rel="noopener noreferrer">- Ronni Davis</a>
           <p>Mobile-first, responsive, author website in HTML/CSS.</p>
         </div>
       </div>
