3f3e91c8fa4aeed8ea32b40ce2a3af0aeb054651
Wanda McCrae <wanda.mccrae@gmail.com>
2025-07-09 10:40:23 -0400

    Added this site as third sample

--- freelance/src/components/Header.js
+++ freelance/src/components/Header.js
@@ -12,7 +12,7 @@ function Header() {
         <div className="contact">
           <NavLink to="/">Home</NavLink>
           <NavLink to="samples">Sample Sites</NavLink>
-          <NavLink to="services">Services</NavLink>
+          <NavLink to="services">Pricing</NavLink>
           <NavLink to="contact">Contact</NavLink>
         </div>
       </div>
--- freelance/src/components/Samples.js
+++ freelance/src/components/Samples.js
@@ -13,11 +13,15 @@ function Samples() {
             href="https://wandamccrae.com/projects/tea-shoppe/" target="_blank" rel="noopener noreferrer">
             - Lotus Blossom Tea Shoppe
           </a>
-          <p>Two-page HTML/CSS site for a local tea shoppe.</p>
+          <p>BASIC WEBSITE for a local tea shoppe.</p>
         </div>
         <div className="sample">
           <a className="sample-project-link" href="https://wandamccrae.com/projects/ronni-davis/" target="_blank" rel="noopener noreferrer">- Ronni Davis</a>
-          <p>Mobile-first, responsive, author website in HTML/CSS.</p>
+          <p>BASIC WEBSITE with more than one EXTRA PAGE (no scripting).</p>
+        </div>
+        <div className="sample">
+          <a className="sample-project-link" href="https://wandamccrae.com/freelance/" target="_blank" rel="noopener noreferrer">- Wanda McCrae &mdash; Freelance Web Developer</a>
+          <p>This very site you are on is an example of a GROWTH WEBSITE with an EXTRA PAGE (no scripting) and a CONTACT PAGE.</p>
         </div>
       </div>
     </div>
