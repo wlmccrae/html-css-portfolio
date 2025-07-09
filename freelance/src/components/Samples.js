2ac495a4a792b883016e83d994f0792056243bcf
Wanda McCrae <wanda.mccrae@gmail.com>
2025-07-09 10:43:23 -0400

    Added NavLink on samples page

--- freelance/src/components/Samples.js
+++ freelance/src/components/Samples.js
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom';
 import '../styles/Samples.css';
 
 function Samples() {
@@ -20,7 +21,9 @@ function Samples() {
           <p>BASIC WEBSITE with more than one EXTRA PAGE (no scripting).</p>
         </div>
         <div className="sample">
-          <a className="sample-project-link" href="https://wandamccrae.com/freelance/" target="_blank" rel="noopener noreferrer">- Wanda McCrae &mdash; Freelance Web Developer</a>
+          <NavLink className="sample-project-link" to="/">
+            - Wanda McCrae &mdash; Freelance Web Developer
+          </NavLink>
           <p>This very site you are on is an example of a GROWTH WEBSITE with an EXTRA PAGE (no scripting) and a CONTACT PAGE.</p>
         </div>
       </div>
